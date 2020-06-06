4b8132103f4f7b0127f5c726121ec10fac483a5e
Nataliya Pogodina <n.pogodina@gmail.com>
2020-06-06 16:22:37 -0700

    Added seeds for dishes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+JSON.parse(File.read('db/seeds/dishes.json')).each do |dish|
+  Dish.create!(dish)
+end
+
+puts "The database now has #{Dish.count} dishes."
