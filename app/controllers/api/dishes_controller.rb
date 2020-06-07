2cdebd7597a8edb2f464da25db0b260007aec3a6
Nataliya Pogodina <n.pogodina@gmail.com>
2020-06-06 17:05:24 -0700

    Added show action to Dishes controller

--- app/controllers/api/dishes_controller.rb
+++ app/controllers/api/dishes_controller.rb
@@ -3,4 +3,20 @@ class Api::DishesController < ApplicationController
     dishes = Dish.order(:name).as_json(only: [:id, :name, :meal, :servings, :recipe])
     render json: dishes, status: :ok
   end
+
+  def show
+    dish = Dish.find_by(id: params[:id])
+
+    if dish
+      render json: dish.as_json(
+        only: [:id, :name, :meal, :servings, :recipe]
+      )
+    else 
+      render json: { errors: ["Not Found"] }, status: :not_found
+    end
+  end
+
+  def dish_params
+    # params.permit(:title, :overview, :release_date, :total_inventory, :available_inventory)
+  end
 end
