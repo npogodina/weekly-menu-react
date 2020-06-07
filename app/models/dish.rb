1efa07d0944e84eda281fdeb31dbc46e21508c2f
Nataliya Pogodina <n.pogodina@gmail.com>
2020-06-07 07:11:03 -0700

    Cleaned up the dish model code

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,4 +1,11 @@
 class Dish < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
+  validates :servings, numericality: { only_integer: true, greater_than: 0 }
+
+  before_save :default_values
+  
+  def default_values
+    self.name = self.name.split.map(&:capitalize).join(' ')
+  end
 end
